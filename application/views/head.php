b91f4842906ed419f95b7769b6a762ca881855ca
software.contreras@gmail.com <software.contreras@gmail.com>
2018-08-03 03:42:17 -0500

    changes on head css add file

--- application/views/head.php
+++ application/views/head.php
@@ -10,12 +10,9 @@
     <meta property="og:description" content="New future network (red de consumo inteligente) es una empresa basada en el consumo de primera necesidad." />
     <meta property="og:url" content="http://rednfn.com/" />
     <meta property="og:site_name" content="NFN" />
+    <meta name="builder-styles-css" content="" id="builder-styles-css">
     <link rel='dns-prefetch' href='//fonts.googleapis.com' />
     <link rel='dns-prefetch' href='//s.w.org' />
-    <script type="text/javascript">
-            window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/themify.me\/demo\/themes\/ultra-app\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.9.7"}};
-            !function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56692,8205,9792,65039],[55357,56692,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
-    </script>
     <style type="text/css">
     img.wp-smiley,
     img.emoji {
@@ -30,18 +27,16 @@
             padding: 0 !important;
     }
     </style>
-    <link rel='stylesheet' id='themify-common-css'  href='https://themify.me/demo/themes/ultra-app/wp-content/themes/themify-ultra/themify/css/themify.common.css?ver=4.9.7' type='text/css' media='all' />
-    <meta name="builder-styles-css" content="" id="builder-styles-css">
-    <link rel='stylesheet' id='theme-style-css'  href='https://themify.me/demo/themes/ultra-app/wp-content/themes/themify-ultra/style.css?ver=2.0.2' type='text/css' media='all' />
-    <link rel='stylesheet' id='themify-media-queries-css'  href='https://themify.me/demo/themes/ultra-app/wp-content/themes/themify-ultra/media-queries.css?ver=2.0.2' type='text/css' media='all' />
-    <link rel='stylesheet' id='themify-skin-css'  href='https://themify.me/demo/themes/ultra-app/wp-content/themes/themify-ultra/skins/app/style.css?ver=4.0.0' type='text/css' media='all' />
+    <link rel='stylesheet' id='themify-common-css'  href='<?php echo site_url().'static/page_front/css/themify.common.css?ver=4.9.7';?>' media='all'/>
+    <link rel='stylesheet' id='theme-style-css'  href='<?php echo site_url().'static/page_front/css/style.css?ver=2.0.2';?>' media='all' />
+    <link rel='stylesheet' id='themify-media-queries-css'  href='<?php echo site_url().'static/page_front/css/media-queries.css?ver=2.0.2';?>' media='all' />
+    <link rel='stylesheet' id='themify-skin-css'  href='<?php echo site_url().'static/page_front/css/app_style.css?ver=4.0.0';?>' media='all' />
     <link rel='stylesheet' id='ultra-header-css'  href='https://themify.me/demo/themes/ultra-app/wp-content/themes/themify-ultra/styles/header-horizontal.css?ver=4.9.7' type='text/css' media='all' />
     <link rel='stylesheet' id='themify-builder-6-generated-css'  href='https://themify.me/demo/themes/ultra-app/files/themify-css/themify-builder-6-generated.css?ver=1530648927' type='text/css' media='all' />
     <link rel='stylesheet' id='themify-customize-css'  href='https://themify.me/demo/themes/ultra-app/files/themify-customizer.css?ver=18.05.29.22.35.02' type='text/css' media='all' />
     <link rel='stylesheet' id='themify-google-fonts-css'  href='https://fonts.googleapis.com/css?family=Raleway%3A300%2C400%2C500%2C600%2C700%2C800%2C900%7CPoppins%3A300%2C400&#038;subset=latin&#038;ver=4.9.7' type='text/css' media='all' />
-    <script type='text/javascript' src='https://themify.me/demo/themes/ultra-app/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
-    <script type='text/javascript' src='https://themify.me/demo/themes/ultra-app/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
-    <link rel='https://api.w.org/' href='https://themify.me/demo/themes/ultra-app/wp-json/' />
+    <script src='<?php echo site_url().'static/page_front/js/jquery.js?ver=1.12.4';?>'></script>
+    <script src='<?php echo site_url().'static/page_front/js/jquery-migrate.min.js?ver=1.4.1';?>'></script>
     <link rel='shortlink' href='http://rednfn.com/' />
     <script src="https://use.fontawesome.com/3aa4a6fd0b.js"></script>
     <script>
@@ -58,10 +53,9 @@
                 33.3333333333% { background-color: #2ea85c; }
                 25% { background-color: #2bb8ed; }
                 20% { background-color: #dd5135; }
-
-                        }
-                        @keyframes themifyAnimatedBG {
-                                0% { background-color: #33baab; }
+        }
+        @keyframes themifyAnimatedBG {
+                0% { background-color: #33baab; }
                 100% { background-color: #e33b9e; }
                 50% { background-color: #4961d7; }
                 33.3333333333% { background-color: #2ea85c; }
